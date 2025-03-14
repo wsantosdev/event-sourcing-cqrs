21c636a4d5dd7243da1b34645ade2f422a746f1e
William Santos <me@wsantos.dev>
2025-03-13 23:36:22 -0300

    test: position withdrawal

--- src/Positions/WSantosDev.EventSourcing.Positions.Test/Commands/WithdrawTest.cs
+++ src/Positions/WSantosDev.EventSourcing.Positions.Test/Commands/WithdrawTest.cs
@@ -2,11 +2,11 @@
 
 namespace WSantosDev.EventSourcing.Positions.Test
 {
-    public sealed class DepositTest : IDisposable
+    public sealed class WithdrawTest : IDisposable
     {
         private readonly Database _databaseSetup;
 
-        public DepositTest()
+        public WithdrawTest()
         {
             _databaseSetup = DatabaseSetupFactory.Create();
         }
@@ -22,13 +22,13 @@ namespace WSantosDev.EventSourcing.Positions.Test
             var symbol = "MSFT";
             var quantity = 10;
 
-            var sut = new Deposit(_databaseSetup.Store);
+            var sut = new Withdraw(_databaseSetup.Store);
 
             //Act
-            var deposited = await sut.ExecuteAsync(new DepositParams(accountId, symbol, quantity));
+            var withdrawn = await sut.ExecuteAsync(new WithdrawParams(accountId, symbol, quantity));
 
             //Assert
-            Assert.True(deposited);
+            Assert.True(withdrawn);
             Assert.Single(await _databaseSetup.ViewDbContext.ByAccountIdAsync(accountId));
         }
 
@@ -42,14 +42,14 @@ namespace WSantosDev.EventSourcing.Positions.Test
             var position = Position.Open(accountId, symbol, quantity);
             await _databaseSetup.Store.StoreAsync(position);
             await _databaseSetup.ViewStore.StoreAsync(PositionView.CreateFrom(position));
-            
-            var sut = new Deposit(_databaseSetup.Store);
+
+            var sut = new Withdraw(_databaseSetup.Store);
 
             //Act
-            var deposited = await sut.ExecuteAsync(new DepositParams(accountId, symbol, quantity));
+            var withdrawn = await sut.ExecuteAsync(new WithdrawParams(accountId, symbol, quantity));
 
             //Assert
-            Assert.True(deposited);
+            Assert.True(withdrawn);
             Assert.Single(await _databaseSetup.ViewDbContext.ByAccountIdAsync(accountId));
         }
     }
