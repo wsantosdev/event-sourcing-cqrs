9c100cc306d26b99bf6f316127e832d2325ae874
William Santos <me@wsantos.dev>
2025-03-21 20:58:03 -0300

    feat: debit

---
+++ src/WebApi/WSantosDev.EventSourcing.WebApi/Accounts/Controllers/DebitController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WSantosDev.EventSourcing.Accounts;
+using WSantosDev.EventSourcing.Accounts.Commands;
+
+namespace WSantosDev.EventSourcing.WebApi.Accounts
+{
+    [Tags("Account")]
+    [Route("api/Account")]
+    [ApiController]
+    public class DebitController(Debit command) : ControllerBase
+    {
+        [HttpPost("Debit")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> Debit(DebitRequest request)
+        {
+            var debited = await command.ExecuteAsync(new DebitParams(Constants.DefaultAccountId, request.Amount));
+            if (debited)
+                return Ok();
+
+            return debited.ErrorValue switch
+            {
+                InvalidAmountError => BadRequest($"Invalid amount. The amount should be greater than zero."),
+                StorageUnavailableError => StatusCode(StatusCodes.Status500InternalServerError, $"Storage unavailable."),
+                _ => StatusCode(StatusCodes.Status500InternalServerError, "Unspecified error."),
+            };
+        }
+    }
+
+    public record struct DebitRequest(decimal Amount);
+}
