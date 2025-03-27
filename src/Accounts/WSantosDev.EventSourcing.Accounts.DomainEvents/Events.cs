68ebaa63aa02b356cf9829fc2c6a59670bf076c9
William Santos <me@wsantos.dev>
2025-03-27 17:32:33 -0300

    removing unnecessary content

--- src/Accounts/WSantosDev.EventSourcing.Accounts.DomainEvents/Events.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using WSantosDev.EventSourcing.Commons.Messaging;
-
-namespace WSantosDev.EventSourcing.Accounts
-{
-    public record AccountOpened(Guid AccountId, decimal InitialDeposit) : IMessage;
-    public record AccountCredited(Guid AccountId, decimal Amount) : IMessage;
-    public record AccountDebited(Guid AccountId, decimal Amount) : IMessage;
-}
