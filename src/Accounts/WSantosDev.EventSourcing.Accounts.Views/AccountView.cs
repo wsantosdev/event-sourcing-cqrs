8c9a84521317ecdda23b4bbc992c33ce99a48e4b
William Santos <me@wsantos.dev>
2025-03-27 17:31:40 -0300

    removing unnecessary content

--- src/Accounts/WSantosDev.EventSourcing.Accounts.Views/AccountView.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace WSantosDev.EventSourcing.Accounts
-{
-    public class AccountView(Guid accountId, decimal balance)
-    {
-        public Guid AccountId { get; init; } = accountId;
-        public decimal Balance { get; set; } = balance;
-    }
-}
